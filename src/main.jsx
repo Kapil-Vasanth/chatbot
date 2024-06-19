87b9bcdcd29cd682cc67dbac24829f4f7135d8bb
kapilvasanth <kapilvasanthbalerao@gmail.com>
2024-06-19 12:03:38 +0530

    Chat Bot Ai

---
+++ src/main.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Home from './pages/Home/Home.jsx'
+import History from './pages/History/History.jsx'
+
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    children: [
+      {
+        path: "history",
+        element: <History />
+      },
+      {
+        path: "/",
+        element: <Home />
+      }
+    ]
+  }
+])
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+    
+  </React.StrictMode>,
+)
